8af5fec660a5804e383220afab77d8f44a36581d
Johnny Freeman <prsjohnny@gmail.com>
2013-09-03 18:25:06 -0400

    re-enabled sessions now that Im working on a more flexible autologin solution

--- server/autologin.js
+++ server/autologin.js
@@ -5,20 +5,18 @@ Accounts.registerLoginHandler(function (options) {
   if (!options.autologin || RequestData.get('alid') === undefined)
     return undefined;
 
-  console.log(RequestData.get('alid'), RequestData.get);
-
   // search for user by autologin id
   var user = Meteor.users.findOne({profile: { autologinId: RequestData.get('alid')}});
   if (!user)
     throw new Meteor.Error(403, "The autologin id '"+RequestData.get('alid')+"' has not been tethered to an account yet");
 
   // add token to user account
-  // var stampedLoginToken = Accounts._generateStampedLoginToken();
-  // Meteor.users.update(user._id, {$push: {'services.resume.loginTokens': stampedLoginToken}});
+  var stampedLoginToken = Accounts._generateStampedLoginToken();
+  Meteor.users.update(user._id, {$push: {'services.resume.loginTokens': stampedLoginToken}});
 
   // return account info
   return {
-    // token: stampedLoginToken.token, 
+    token: stampedLoginToken.token, 
     id: user._id
   };
 

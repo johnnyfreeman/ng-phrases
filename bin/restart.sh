b8221fc80fd8cc0d9b1f48822fc32bb8b56608a1
Johnny Freeman <prsjohnny@gmail.com>
2013-09-17 16:10:01 -0400

    added kill open process logic

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,2 +1,12 @@
+echo "The following node processes were stopped:"
+ps aux | grep " node " | grep -v grep
+nodepids=$(ps aux | grep " node " | grep -v grep | cut -c10-15)
+
+for nodepid in ${nodepids[@]}
+do
+  echo "PID :"$nodepid
+  kill -9 $nodepid
+done
+
 # start meteor
 mrt

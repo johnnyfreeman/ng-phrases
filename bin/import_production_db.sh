124f35802320c68a772c79e8e069926a08307a68
Johnny Freeman <prsjohnny@gmail.com>
2013-09-20 17:47:15 -0400

    add the importdb script to the bin folder so that I can use it locally... anywhere

---
+++ bin/import_production_db.sh
@@ -0,0 +1,20 @@
+echo "tunneling into ng-phrases server and backing up production db..."
+ssh 192.237.249.38 'cd ~; rm -rfv ~/dump/meteor; mongodump -v --db meteor --port 3002'
+
+echo "deleting previous (local) backup..."
+rm -rfv ~/dump/meteor
+
+echo "moving production dump to local machine..."
+scp -r 192.237.249.38:~/dump/ ~/
+
+echo "changing directory to ~/dump/meteor/ ..."
+cd ~/dump/meteor/
+
+echo "renaming all .json files to .bson to prepare for mongorestore"
+rename 's/.json$/.bson/' *.json -v
+
+# temp fix
+rm -v ./*.metadata.bson
+
+echo "restoring production dump..."
+mongorestore -v --host localhost --port 3002 --db meteor .
